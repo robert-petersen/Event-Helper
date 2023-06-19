9eddcd952f01e4edb71a7dc118e66dab52437599
Robert Petersen <robert.petersen808@gmail.com>
2023-06-18 19:06:09 -0500

    Some formatting changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const {Client, IntentsBitField, EmbedBuilder} = require("discord.js");
+const { Client, IntentsBitField, EmbedBuilder } = require("discord.js");
 
 const client = new Client({
   intents: [
@@ -7,7 +7,7 @@ const client = new Client({
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
-  ]
+  ],
 });
 
 client.on("ready", (c) => {
@@ -15,29 +15,31 @@ client.on("ready", (c) => {
 });
 
 client.on("interactionCreate", (interaction) => {
-  if(!interaction.isChatInputCommand()) return;
-  if(interaction.commandName === "add") {
+  if (!interaction.isChatInputCommand()) return;
+  if (interaction.commandName === "add") {
     const num1 = interaction.options.get("first-number").value;
     const num2 = interaction.options.get("second-number").value;
     interaction.reply(`The sum is: ${num1 + num2}`);
   }
-  if(interaction.commandName === "embed") {
+  if (interaction.commandName === "embed") {
     const embed = new EmbedBuilder()
       .setTitle("Embed Title")
       .setDescription("This is an embed description")
       .setColor("Random")
-      .addFields({ 
+      .addFields({
         name: "field title",
         value: "Some random value",
         inline: true,
-      })
+      });
     interaction.reply({ embeds: [embed] });
   }
 });
 
 client.on("messageCreate", (message) => {
-  if(message.author.bot) {return;}
-  if(message.content == "hi") {
+  if (message.author.bot) {
+    return;
+  }
+  if (message.content == "hi") {
     message.reply("Hello World!");
   }
 });
--- src/registerCommands.js
+++ src/registerCommands.js
@@ -30,27 +30,26 @@ const commands = [
             name: "three",
             value: 3,
           },
-        ]
+        ],
       },
       {
         name: "second-number",
         description: "The second number.",
         type: ApplicationCommandOptionType.Number,
         required: true,
-      }
-    ]
-  }
+      },
+    ],
+  },
 ];
 
-const rest = new REST({version: "10"}).setToken(process.env.TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
     console.log("Registering slash commands...");
-    await rest.put(
-      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), 
-        {body: commands}
-    );
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body: commands,
+    });
     console.log("Slash commands were registered successfully!");
   } catch (error) {
     console.log("There was an error: ", error);
