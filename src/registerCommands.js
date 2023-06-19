9ed74b982707a0e6a5d2d6a6bec789112b6ca8ca
Robert Petersen <robert.petersen808@gmail.com>
2023-06-18 21:44:38 -0500

    Created commands folder / added a command handler / improved registerCommands

--- src/registerCommands.js
+++ src/registerCommands.js
@@ -1,57 +1,56 @@
 require("dotenv").config();
-const { REST, Routes, ApplicationCommandOptionType } = require("discord.js");
-const CLIENT_ID = process.env.CLIENT_ID;
-const GUILD_ID = process.env.GUILD_ID;
+const { REST, Routes } = require("discord.js");
+const { CLIENT_ID, TOKEN } = process.env;
+const fs = require("node:fs");
+const path = require("node:path");
 
-const commands = [
-  {
-    name: "embed",
-    description: "Sends an embed!",
-  },
-  {
-    name: "add",
-    description: "Adds two numbers.",
-    options: [
-      {
-        name: "first-number",
-        description: "The first number.",
-        type: ApplicationCommandOptionType.Number,
-        required: true,
-        choices: [
-          {
-            name: "one",
-            value: 1,
-          },
-          {
-            name: "two",
-            value: 2,
-          },
-          {
-            name: "three",
-            value: 3,
-          },
-        ],
-      },
-      {
-        name: "second-number",
-        description: "The second number.",
-        type: ApplicationCommandOptionType.Number,
-        required: true,
-      },
-    ],
-  },
-];
+const commands = [];
+// Grab all the command files from the commands directory you created earlier
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
 
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+for (const folder of commandFolders) {
+  // Grab all the command files from the commands directory you created earlier
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      commands.push(command.data.toJSON());
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
 
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(TOKEN);
+
+// and deploy your commands!
 (async () => {
   try {
-    console.log("Registering slash commands...");
-    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
-      body: commands,
-    });
-    console.log("Slash commands were registered successfully!");
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID),
+      // ^^ add GUILD_ID to test in dev server only ^^
+      { body: commands }
+    );
+
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
   } catch (error) {
-    console.log("There was an error: ", error);
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
   }
 })();
